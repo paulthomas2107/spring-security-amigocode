1301e0015d98087aef72a6d6cd72ad1c7a2486a4
Paul Thomas <paulthomas2107@gmail.com>
2022-12-07 13:21:03 +0000

    More..

--- src/main/java/com/pault/springsecurity/controllers/GreetingsController.java
+++ src/main/java/com/pault/springsecurity/controllers/GreetingsController.java
@@ -1,4 +1,4 @@
-package com.pault.springsecurity;
+package com.pault.springsecurity.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
